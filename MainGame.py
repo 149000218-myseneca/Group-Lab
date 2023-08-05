afb533a9296e9dfb1b735da01625f087b280b7fd
149000218-myseneca <ppanchal20@myseneca.ca>
2023-08-05 10:46:56 -0400

    Add Rock, Paper, Scissors game

---
+++ MainGame.py
@@ -0,0 +1,33 @@
+import random
+
+options = ["rock", "paper", "scissors"]
+computerOption = random.choice(options)
+
+print("Welcome to Rock, Paper, Scissors!")
+playerChoice = input("Enter your choice (rock/paper/scissors): ").lower()
+
+print("Your choice:", playerChoice)
+print("Computer's choice:", computerOption)
+
+if playerChoice in options:
+    if playerChoice == computerOption:
+        print("It's a tie!")
+    elif playerChoice == "rock":
+        if computerOption == "scissors":
+            print("You win! Rock crushes scissors.")
+        else:
+            print("Computer wins! Paper covers rock.")
+    elif playerChoice == "paper":
+        if computerOption == "rock":
+            print("You win! Paper covers rock.")
+        else:
+            print("Computer wins! Scissors cut paper.")
+    elif playerChoice == "scissors":
+        if computerOption == "paper":
+            print("You win! Scissors cut paper.")
+        else:
+            print("Computer wins! Rock crushes scissors.")
+else:
+    print("Invalid choice. Please choose from rock, paper, or scissors.")
+
+print("Thanks for playing!")
